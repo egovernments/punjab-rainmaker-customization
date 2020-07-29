42e8f2e17e1ce9bb63aa0d423bfccc862c5899c5
narendrabandhamneni-wt-egov <narendrabandhamaneni@egovernments.org>
2020-07-29 19:40:43 +0530

    Property search changes

--- migration/ws/config/common_config.py
+++ migration/ws/config/common_config.py
@@ -5,8 +5,9 @@ def load_config():
     config.URL_LOGIN = config.HOST + "/user/oauth/token"
 
     config.URL_BILLING_SLAB_CREATE = config.HOST + "/pt-calculator-v2/billingslab/_create"
-
     config.TENANT_ID = config.TENANT + "." + config.CITY_NAME.lower()
-
+    config.NO_MOBILE_NUMBER="No mobile number found"
+    config.URL_PROPERTY_SEARCH=config.HOST + "/property-services/property/_search"
+    config.NO_PROPERTY_MESSAGE="No property found"
 
 load_config()
