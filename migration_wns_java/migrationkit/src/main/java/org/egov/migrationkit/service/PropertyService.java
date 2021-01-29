70a3d83814531d30bfae4d301c2afde36dd41c66
KarunaNidhan-egov <karuna.nidhan@egovernments.org>
2021-01-29 15:02:11 +0530

    PI-1184: Property Service Integrartion

--- migration_wns_java/migrationkit/src/main/java/org/egov/migrationkit/service/PropertyService.java
+++ migration_wns_java/migrationkit/src/main/java/org/egov/migrationkit/service/PropertyService.java
@@ -10,6 +10,11 @@ public class PropertyService {
 	{
 		
 		String uuid="0af72ed5-0df3-41de-8749-7275fbfacfe2";
+		//write an api call search pt
+				//if property found compare with owner name,father name etc.
+				//if found return property id
+				// if not found create a property from response return the property id
+				
 		return uuid;
 	}
 
