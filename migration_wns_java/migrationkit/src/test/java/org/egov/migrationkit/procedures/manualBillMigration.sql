e126d8a2a7b049f2c42aca3e39c0f9db64f74b93
Manikanta <manikanta@egovernments.org>
2021-04-25 18:46:30 +0530

    added updating demand

--- migration_wns_java/migrationkit/src/test/java/org/egov/migrationkit/procedures/manualBillMigration.sql
+++ migration_wns_java/migrationkit/src/test/java/org/egov/migrationkit/procedures/manualBillMigration.sql
@@ -1,9 +1,11 @@
- 
+--set search_path to fazilka 
 create or replace function migrate_manual_bills( tenantId varchar)
    returns text as $$ 
 declare 
          billId varchar(64);
 	 billdetailid  varchar(64);
+   demandId_digit varchar(64);
+   demand_detail_digit	varchar(64);
 	 mobilenumber varchar(10) default '9999119999';
 	 titles text default 'Success';
 	 service varchar(64);
@@ -14,7 +16,7 @@ declare
 	 cur_bills cursor for
 	 		 select * from eg_bill where id_bill_type=1 and service_code in ('WT','STAX') 
 	 		 union
-		   select bill.* from eg_bill bill, egcl_collectionheader ch where ch.referencenumber::bigint=bill.id and bill.service_code in ('WT','STAX')  ;
+		   select bill.* from eg_bill bill, egcl_collectionheader ch where ch.referencenumber::bigint=bill.id and bill.service_code in ('WT','STAX')  and id_bill_type!=1  ;
    
 
 begin
@@ -52,8 +54,26 @@ billId, tenantId, rec.citizen_name, rec.citizen_address, rec.emailid, True, Fals
             '6ccc8719-5b0a-4d24-924e-ec6d2a674b28', Extract(epoch FROM rec.modified_date) * 1000, mobilenumber, 'ACTIVE', '{"manualmigratedbill":true}') ;
 
  select * from eg_bill_details detail ,eg_demand_reason reason,eg_installment_master inst  
-where reason.id_installment=inst.id and detail.id_demand_reason=reason.id and   id_bill=rec.id into bill_detail ;
+where reason.id_installment=inst.id and detail.id_demand_reason=reason.id and  detail.id_installment=inst.id and  id_bill=rec.id into bill_detail ;
+
 SELECT uuid_in(md5(random()::text || clock_timestamp()::text)::cstring) into billdetailid ;
+
+
+select demand.id
+from public.egbs_demand_v1 demand where    
+  demand.consumercode=rec.consumer_id  
+and demand.taxperiodfrom= (select Extract(epoch FROM max(inst.start_date)   ) * 1000  from eg_bill_details bd, eg_demand_reason dr, eg_installment_master inst, eg_demand_reason_master drm
+where dr.id_installment=inst.id and bd.id_demand_reason=dr.id and bd.id_installment=inst.id and drm.id=dr.id_demand_reason_master
+and drm.code not like '%ADVANCE' and id_bill=rec.id)
+and demand.taxperiodto=Extract(epoch FROM bill_detail.end_date    ) * 1000 - 19800000
+into demandId_digit ;
+
+if( demandId_digit is null) then 
+
+select demand.id   from public.egbs_demand_v1 demand where demand.consumercode=rec.consumer_id  
+and  Extract(epoch FROM rec.issue_date    ) * 1000 between demand.taxperiodfrom and demand.taxperiodto into demandId_digit;
+end if;
+
 
 INSERT INTO public.egbs_billdetail_v1(
             id, tenantid, billid, businessservice, billno, billdate, consumercode, 
@@ -66,11 +86,11 @@ INSERT INTO public.egbs_billdetail_v1(
             null, null, rec.min_amt_payable, rec.total_amount, 
             null, null, null, '6ccc8719-5b0a-4d24-924e-ec6d2a674b28', 
             Extract(epoch FROM rec.create_date) * 1000,  '6ccc8719-5b0a-4d24-924e-ec6d2a674b28', Extract(epoch FROM rec.create_date) * 1000, 
-           null, null, Extract(epoch FROM bill_detail.start_date) * 1000, Extract(epoch FROM bill_detail.end_date) * 1000, null, null, 
+           null, null, Extract(epoch FROM bill_detail.start_date) * 1000, Extract(epoch FROM bill_detail.end_date) * 1000, demandId_digit, null, 
             Extract(epoch FROM rec.last_date) * 1000, '{"manualmigratedbill":true}');
 
 for props in (select * from eg_bill_details detail ,eg_demand_reason reason,eg_installment_master inst ,eg_demand_reason_master master  
-where reason.id_installment=inst.id and detail.id_demand_reason=reason.id and master.id=reason.id_demand_reason_master and   id_bill=rec.id   )
+where reason.id_installment=inst.id and detail.id_demand_reason=reason.id and master.id=reason.id_demand_reason_master  and     id_bill=rec.id   )
 Loop
  begin
 
@@ -99,6 +119,7 @@ if( props.code = 'STAXROADCUTTING') then head:='SW_ROAD_CUTTING_CHARGE' ; end if
 if( props.code = 'STAXAPPLICATION') then head:='SW_FORM_FEE' ; end if;
 if( props.code = 'STAXSUPERVISION') then head:='SW_SUPERVISION_CHARGE' ; end if;
 if( props.code = 'SEWERAGETAX') then head:='SW_CHARGE' ; end if;
+ 
 if( props.code = 'SWTAXADJUSTMENT') then head:='SW_TAX_ADJUSTMENT' ; end if;
 if( props.code = 'PENALTY' and  service = 'SW') then head:='SW_TIME_PENALTY' ; end if;
 if( props.code = 'TITLETRANSFERFEE') then head:='SW_TITLE_TRANSFER_FEE' ; end if;
@@ -108,7 +129,12 @@ if( props.code = 'DOORTODOORCOLLECTIONCHARGES') then head:='SW_DOOR_TO_DOOR_COLL
 if( props.code = 'DONATIONCHARGE') then head:='SW_DONATION_CHARGE' ; end if;
 if( props.code = 'INSPECTIONCHARGE') then head:='SW_INSPECTION_CHARGE' ; end if;
 if( props.code = 'ESTIMATIONCHARGE') then head:='SW_ESTIMATION_CHARGE' ; end if;
-	     
+ 
+select id from public.egbs_demanddetail_v1 where demandid =demandId_digit
+and taxheadcode=head into demand_detail_digit;
+
+
+ 
          INSERT INTO public.egbs_billaccountdetail_v1(
             id, tenantid, billdetail, glcode, orderno, accountdescription, 
             creditamount, debitamount, isactualdemand, purpose, createdby, 
@@ -118,7 +144,7 @@ if( props.code = 'ESTIMATIONCHARGE') then head:='SW_ESTIMATION_CHARGE' ; end if;
 	   props.description, 
             props.cr_amount, props.dr_amount, False, props.purpose, '6ccc8719-5b0a-4d24-924e-ec6d2a674b28', 
              Extract(epoch FROM rec.create_date) * 1000, '6ccc8719-5b0a-4d24-924e-ec6d2a674b28', Extract(epoch FROM rec.create_date) * 1000, props.cr_amount, 
-           head, props.cr_amount, 0, null, '{"manualmigratedbill":true}');
+           head, props.cr_amount, 0, demand_detail_digit, '{"manualmigratedbill":true}');
    end ;
    end Loop;
            
@@ -137,7 +163,12 @@ end; $$
 language plpgsql;
 
 
-select migrate_manual_bills('pb.fazilka');
+
+
+--select migrate_manual_bills('pb.fazilka');
+--delete from public.egbs_billaccountdetail_v1 where additionaldetails ='{"manualmigratedbill":true}';
+--delete from public.egbs_billdetail_v1 where additionaldetails ='{"manualmigratedbill":true}';
+--delete from public.egbs_bill_v1 where additionaldetails ='{"manualmigratedbill":true}';
 
 
 
